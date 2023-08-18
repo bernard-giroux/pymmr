7da2335847ea8250774f8e6fd8512cfe16c3d390
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2023-08-18 10:17:41 -0400

    reformat with black

--- examples/mmr_surface.py
+++ examples/mmr_surface.py
@@ -4,17 +4,17 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-sys.path.append('/Users/giroux/GitHub/pymmr')
-sys.path.append('/Users/giroux/GitHub/pymumps')
+sys.path.append("/Users/giroux/GitHub/pymmr")
+sys.path.append("/Users/giroux/GitHub/pymumps")
 
 from pymmr.finite_volume import calc_padding, GridFV
 from pymmr.mmr import GridMMR
 from pymmr.inversion import Inversion
 
 
-x = np.arange(-60.0*16, 60.01*16, 60)
-y = np.arange(-60.0*16, 60.01*16, 60)
-z = np.arange(0.0, 60.01*16, 60)
+x = np.arange(-60.0 * 16, 60.01 * 16, 60)
+y = np.arange(-60.0 * 16, 60.01 * 16, 60)
+z = np.arange(0.0, 60.01 * 16, 60)
 
 pad = np.cumsum(calc_padding(60.0, 10))
 
@@ -23,17 +23,20 @@ y = np.r_[y[0] - pad[::-1], y, y[-1] + pad]
 z = np.r_[z, z[-1] + pad]
 
 g = GridMMR(x, y, z)
-g.set_solver('mumps')
+g.set_solver("mumps")
 
 
-c1c2 = np.array([[-600.0, 0.0, 0.0, 600.0, 0.0, 0.0],
-                 [0.0, -600.0, 0.0, 0.0, 600.0, 0.0]])
+c1c2 = np.array(
+    [[-600.0, 0.0, 0.0, 600.0, 0.0, 0.0], [0.0, -600.0, 0.0, 0.0, 600.0, 0.0]]
+)
 
 xo = np.linspace(-400, 400, 26)
 yo = np.linspace(-400, 400, 26)
-xo = np.c_[np.kron(xo.reshape(-1, 1), np.ones((yo.size, 1))),
-           np.kron(np.ones((xo.size, 1)), yo.reshape(-1, 1)),
-           np.zeros((xo.size*yo.size, 1))]
+xo = np.c_[
+    np.kron(xo.reshape(-1, 1), np.ones((yo.size, 1))),
+    np.kron(np.ones((xo.size, 1)), yo.reshape(-1, 1)),
+    np.zeros((xo.size * yo.size, 1)),
+]
 
 tmp = np.kron(c1c2, np.ones((xo.shape[0], 1)))
 xo = np.kron(np.ones((c1c2.shape[0], 1)), xo)
@@ -64,9 +67,9 @@ g.apply_bc = True
 
 sigma = 0.001 + np.zeros((g.gdc.nc,))
 
-ix = np.where(np.logical_and(g.xc>-200, g.xc<200))[0]
-iy = np.where(np.logical_and(g.yc>-200, g.yc<200))[0]
-iz = np.where(np.logical_and(g.gdc.zc>80, g.gdc.zc<480))[0]
+ix = np.where(np.logical_and(g.xc > -200, g.xc < 200))[0]
+iy = np.where(np.logical_and(g.yc > -200, g.yc < 200))[0]
+iz = np.where(np.logical_and(g.gdc.zc > 80, g.gdc.zc < 480))[0]
 sigma[g.gdc.ind(ix, iy, iz)] = 0.1
 
 dobs1 = g.fwd_mod(sigma)
@@ -97,7 +100,7 @@ g2 = GridFV(x, y, z)
 
 fields = {}
 for i in range(len(S_save)):
-    name = 'iteration {0:d}'.format(i+1)
+    name = "iteration {0:d}".format(i + 1)
     fields[name] = S_save[i]
 
-g2.toVTK(fields, 'mmr_surface')
+g2.toVTK(fields, "mmr_surface")
