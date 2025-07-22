f9ea1271f806f5a399d82b23d425e4e1f6d6e8d6
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2025-07-22 13:36:03 -0400

    updated to build on linux

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+
+import platform
 import numpy as np
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 from Cython.Build import cythonize
 
-include_dirs = ['/opt/local/libexec/boost/1.81/include', np.get_include()]
+if platform.system() == 'Darwin':
+  include_dirs = ['/opt/local/libexec/boost/1.81/include', np.get_include()]
+elif if platform.system() == 'Linux':
+  include_dirs = ['../boost_1_88_0', np.get_include()]
 
 setup(
     ext_modules = cythonize([
